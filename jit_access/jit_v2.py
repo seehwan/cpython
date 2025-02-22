2c615d1ca0e6a0e760ae244b7df1e6d0a1ec3868
Seehwan yoo <seehwan.yoo@gmail.com>
2025-02-23 08:07:58 +0900

    jit access
    you can access jit code buffer using cpython module.
    CPython module allows access to code buffer, and we can create an alias
    that modifies the code.
    
    python setup.py build_ext --inplace
    python jit_v1.py
    
    in the first run, test_function cumulatively adds/subtracts value from 1~20
    in the second run, test_function's value changes.
    it is because call to _PyLong_Add is changed to _PyLong_Subtract

---
+++ jit_access/jit_v2.py
@@ -0,0 +1,42 @@
+import jitaccess
+import dis
+
+def get_value(index):
+    if index < 10000:
+        return index
+    else:
+        return -1
+
+
+def test_function():
+    result = 0
+    result2 = 0
+    for i in range(20):
+        value = get_value(i)
+        result = value + result
+        result2 = result2 - value
+        print(f"i: {i} res: {result} res2: {result2}")
+    return result
+
+
+raw_bytecode = test_function.__code__.co_code
+print("\nBytecode with Disassembled Instructions:")
+print("Offset | Raw Bytecode | Instruction")
+instructions = dis.get_instructions(test_function)
+for instr in instructions:
+    raw = raw_bytecode[instr.offset:instr.offset+2]  # Get bytecode slice
+    raw_hex = " ".join(f"{byte:02x}" for byte in raw)
+    print(f"{instr.offset:6} | {raw_hex:<12} | {instr.opname} {instr.argrepr}")
+
+test_function()
+
+jitaccess.attack_alias(test_function, 116)
+# following code invalidates jit execution for test_function
+jit_code = jitaccess.get_executor_code_address(test_function, 98)
+print("jit code found: ", (jit_code))
+
+try:
+    test_function()
+except Exception as e:
+    print(e)
+    print("test_function() failed as expected")
---
+++ jit_access/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, Extension
+
+
+module = Extension(
+    'jitaccess',
+    sources=['jitaccess.c'],
+    include_dirs=[
+        '/proj/sekvmii-PG0/mydata/cpython/build',
+        '/proj/sekvmii-PG0/mydata/cpython/Include',
+        '/proj/sekvmii-PG0/mydata/cpython/Include/internal'
+    ],
+    extra_compile_args=['-DPy_BUILD_CORE', '-g'],
+    extra_link_args=['-g']
+)
+
+setup(
+    name='jitaccess',
+    version='1.0',
+    description='Access CPython JIT executor buffer',
+    ext_modules=[module],
+)
+
+
+
